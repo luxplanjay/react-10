7fe8b884dfc130323f983d18d3da24476126745d
Alexander Repeta <alexander.repeta@gmail.com>
2020-01-17 21:05:23 +0200

    Fix typo

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 function Product({ imgUrl, name, price, quantity }) {
   return (
     <div>
-      {undefined}
       <img src={imgUrl} alt={name} width="640" />
       <h2>{name}</h2>
       <p>Price: {price}$</p>
